d736319151f158a8ad03977b47825777cb73e7f6
sonal-alt <74061846+sonal-alt@users.noreply.github.com>
2021-01-18 13:40:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('Hello World!');
+function sq(){
+ result=document.getElementById("num").value;
+ Number=(Math.sqrt(result))
+document.getElementById("paragraph").innerHTML=(Number);
+};
